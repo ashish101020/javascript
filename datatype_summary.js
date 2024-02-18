89746b07c668b021dcf4b2b9cd84d5b1a9cf94f8
ashish <guptashish101020@gmail.com>
2024-02-18 10:47:29 +0530

    datatype

---
+++ datatype_summary.js
@@ -0,0 +1,2 @@
+const id = Symbol('123')
+const anotherId = Symbol('123')
--- ram.js
+++ ram.js
@@ -1,1 +1,8 @@
-console.log("ram")
+console.log("ram");
+console.log("05">=4);
+console.log("03">=4);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
